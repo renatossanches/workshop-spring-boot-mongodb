7030767597b5d1eb024a2396c9db6cecc8e246e1
renatossanches <renato.sanches2004@gmail.com>
2024-10-11 14:21:20 -0300

    Primeiro commit

---
+++ src/test/java/com/renatossanches/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renatossanches.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
