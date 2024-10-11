0cb81d69746b2adb658ba57dd55713faa51de7f3
renatossanches <renato.sanches2004@gmail.com>
2024-10-11 18:11:09 -0300

    Inserção de usuário com POST

--- src/main/java/com/renatossanches/workshopmongo/services/UserService.java
+++ src/main/java/com/renatossanches/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.renatossanches.workshopmongo.domain.User;
+import com.renatossanches.workshopmongo.dto.UserDTO;
 import com.renatossanches.workshopmongo.repository.UserRepository;
 import com.renatossanches.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,12 @@ public class UserService {
 	    return userOpt.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
+	
 }
