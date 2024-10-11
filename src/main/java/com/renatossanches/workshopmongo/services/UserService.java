5af6848029e8dc6353a447652890914d4204581c
renatossanches <renato.sanches2004@gmail.com>
2024-10-11 17:35:54 -0300

    Obtendo um usuário por id

--- src/main/java/com/renatossanches/workshopmongo/services/UserService.java
+++ src/main/java/com/renatossanches/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.renatossanches.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.renatossanches.workshopmongo.domain.User;
 import com.renatossanches.workshopmongo.repository.UserRepository;
+import com.renatossanches.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,10 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+	    Optional<User> userOpt = repo.findById(id);
+	    return userOpt.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+
 }
